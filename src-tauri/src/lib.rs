43a1a8694edd1712b697fbd75479488f138f400c
J222 <baram789456@gmail.com>
2024-12-18 16:29:07 +0900

    Fix: Disable prod dev tool

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -7,12 +7,12 @@ pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_shell::init())
         .setup(|app| {
-            // #[cfg(debug_assertions)] // only include this code on debug builds
-            // {
+            #[cfg(debug_assertions)] // only include this code on debug builds
+            {
                 let window = app.get_webview_window("main").unwrap();
                 window.open_devtools();
                 window.close_devtools();
-            // }
+            }
             Ok(())
         })
         .invoke_handler(tauri::generate_handler![event::get_system_info])
