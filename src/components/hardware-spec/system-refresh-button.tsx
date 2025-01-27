579142dbfbe4c240a5727cfb9e77b20f906ff3b7
J222 <baram789456@gmail.com>
2025-01-27 19:21:23 +0900

    fix: system refresh

--- src/components/hardware-spec/system-refresh-button.tsx
+++ src/components/hardware-spec/system-refresh-button.tsx
@@ -3,12 +3,8 @@ import { Button, ButtonProps } from '@/components/ui/button';
 type Props = ButtonProps & { className?: string };
 
 export default function SystemRefreshButton(props: Props) {
-  const handleErrorTest = () => {
-    throw new Error('Sentry test error.');
-  };
-
   return (
-    <Button type="button" variant="outline" size="lg" {...props} onClick={handleErrorTest}>
+    <Button type="button" variant="outline" size="lg" {...props}>
       컴퓨터 정보 새로고침
     </Button>
   );
