1a4fa5b520970c6e6663a8e37eccafac4e2312d4
J222 <baram789456@gmail.com>
2025-01-29 21:43:32 +0900

    fix: custom protocol -> localhost

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,25 +1,35 @@
-use tauri::{Manager, webview::WebviewWindowBuilder, WebviewUrl};
+use tauri::{utils::config::FrontendDist, webview::WebviewWindowBuilder, Url, WebviewUrl};
 
 mod event;
 pub mod system;
 
+const WINDOW_TITLE: &str = "픽업컴";
+const WINDOW_WIDTH: f64 = 800.0;
+const WINDOW_HEIGHT: f64 = 600.0;
+
 pub fn run() {
     let port = portpicker::pick_unused_port().expect("failed to find unused port");
-    let url = format!("http://localhost:{}", port).parse().unwrap();
+    let mut context = tauri::generate_context!();
+    let url: Url   = format!("http://localhost:{}", port).parse().unwrap();
+    let window_url = WebviewUrl::External(url.clone());
+    // IPC를 활성화하려면 URL을 재설정해야 합니다.
+    let dist = context.config_mut().build.frontend_dist.as_mut().unwrap();
+    *dist = FrontendDist::Url(url.clone());
+    
 
     tauri::Builder::default()
         .plugin(tauri_plugin_localhost::Builder::new(port).build())
         .plugin(tauri_plugin_http::init())
         .plugin(tauri_plugin_store::Builder::new().build())
         .plugin(tauri_plugin_shell::init())
-        .setup(|app| {
-            let window = WebviewWindowBuilder::new(app, "main", WebviewUrl::External(url))
-            .title("PickupCom")
+        .setup(move |app| {
+            let window = WebviewWindowBuilder::new(app, "main", window_url)
+            .title(WINDOW_TITLE)
+            .min_inner_size(WINDOW_WIDTH, WINDOW_HEIGHT)
             .build()?;
 
             #[cfg(debug_assertions)] // only include this code on debug builds
             {
-                // let window = app.get_webview_window("main").unwrap();
                 window.open_devtools();
                 window.close_devtools();
             }
@@ -27,6 +37,6 @@ pub fn run() {
             Ok(())
         })
         .invoke_handler(tauri::generate_handler![event::get_system_info])
-        .run(tauri::generate_context!())
+        .run(context)
         .expect("error while running tauri application");
 }
